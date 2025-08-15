cdf704cbec3480f3a867ad79436e667d7818bd14
sheena <syeongsn@gmail.com>
2025-08-15 20:21:24 +0800

    added black border around counter text

--- js/utilities.js
+++ js/utilities.js
@@ -31,8 +31,6 @@ function determineWinner({ player, enemy, timerID }) {
   document.querySelector("#enemyPoints").innerHTML = "Points: " + enemyPoints;
 
   if (playerPoints === 3 || enemyPoints === 3) {
-    document.querySelector("#displayText").style.fontSize =
-        "20px";
     if (playerPoints > enemyPoints) {
       document.querySelector("#displayText").innerHTML =
         "P1 WINS";
